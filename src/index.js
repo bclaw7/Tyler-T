898cb0b9f89da929efc370c2a212eaea527bfb3e
bclaw <bclaw8300@gmail.com>
2021-07-13 11:33:45 -0600

    added firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {ProductProvider} from './context';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ProductProvider } from './context';
 
 ReactDOM.render(
-    <ProductProvider>
+  <ProductProvider>
     <Router>
       <App />
     </Router>
-    </ProductProvider>,
+  </ProductProvider>,
   document.getElementById('root')
 );
-
